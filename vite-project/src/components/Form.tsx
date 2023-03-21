cd5385a0d5a7967032a5048b832a729fc34464d9
Maria Samoilova <mariya_baranova@list.ru>
2023-03-21 15:06:52 +0300

    feat: form layout

--- vite-project/src/components/Form.tsx
+++ vite-project/src/components/Form.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler } from 'react'
+import React from 'react'
 import '../styles/Form.css'
 
 type formProps = {};
@@ -19,7 +19,7 @@ class Form extends React.Component<formProps, formState> {
         this.handleForm = this.handleForm.bind(this);
     }
 
-    handleForm = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    handleForm = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('hi');
     }
@@ -27,15 +27,60 @@ class Form extends React.Component<formProps, formState> {
     render() {
         return (
             <div className='form_content'>
-                <form>
-                    <input type='text'></input>
-                    <input type='date'></input>
-                    <input type='select'></input>
-                    <input type='checkbox'></input>
-                    <input type='switcher'></input>
-                    <input type='file'></input>
-                    <button onClick={ this.handleForm }>Submit </button>
-                </form>
+                <fieldset className="form_fieldset">
+                    <legend>User info</legend>
+                    <form onSubmit={ this.handleForm }>
+                        <p></p>
+                        
+                        <label>
+                            Name:
+                            <input type="text" name="name" />
+                        </label>
+
+                        <p></p>
+
+                        <label>
+                            Birthday:
+                            <input type="date" name="date" />
+                        </label>
+
+                        <p></p>
+                        
+                        <span>Country</span>
+                        <select>
+                            <option value="Belarus">Belarus</option>
+                            <option value="Kazakhstan">Kazakhstan</option>
+                            <option value="Kyrgyzstan">Kyrgyzstan</option>
+                            <option value="Russia">Russia</option>
+                            <option value="Ukraine">Ukraine</option>
+                            <option value="Uzbekistan">Uzbekistan</option>
+                        </select>
+
+                        
+                        <p></p>
+                        <span>Gender</span>
+                        <label>
+                            <input type='radio' name="sex"></input>
+                            male
+                        </label>
+                        <label>
+                            <input type='radio' name="sex"></input>
+                            female
+                        </label>
+
+                        <p></p>
+                        <span>profile picture</span>
+                        <input type='file'></input>
+
+                        <p></p>
+                        <input type='checkbox'></input>
+                        <span>I consent to my personal data</span>
+
+
+                        <p></p>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </fieldset>
             </div>
         )
     }
