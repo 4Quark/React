e44d05a74b4b738ebd70dbd355a6604257e16930
Maria Samoilova <mariya_baranova@list.ru>
2023-04-05 22:20:13 +0300

    feat: search result

--- vite-project/src/pages/homepage.tsx
+++ vite-project/src/pages/homepage.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import Card from '../components/Card';
 import Search from '../components/Search';
-import data from '../data/data.json';
+import SearchResult from '../components/SearchResult';
 
 const Homepage = () => {
-  const cards = [];
-  for (let i = 0; i < 30; i++) {
-    if (data.products[i]) cards.push(data.products[i]);
-  }
-
   return (
     <div className="home_container">
       <h1>RS School React</h1>
       <Search />
-      <div className="cards_container">
-        {cards.map((card) => (
-          <Card post={card} key={card.id} />
-        ))}
-      </div>
+      <SearchResult />
     </div>
   );
 };
