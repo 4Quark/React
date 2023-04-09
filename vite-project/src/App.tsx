78e65b02da5623f468914caefcf7ad97f20b0978
Maria Samoilova <mariya_baranova@list.ru>
2023-04-09 16:49:16 +0300

    fix: test App

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { Routes, Route, NavLink, BrowserRouter } from 'react-router-dom';
 import About from './pages/about';
 import Homepage from './pages/homepage';
 import NotFound from './pages/notFound';
@@ -8,37 +8,39 @@ import FormPage from './pages/form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header">
-        <h1 className="logo">ReactAPP</h1>
-        <NavLink className="navigation" to="/">
-          Home
-        </NavLink>
-        <NavLink className="navigation" to="/about">
-          About
-        </NavLink>
-        <NavLink className="navigation" to="/counter">
-          Counter
-        </NavLink>
-        <NavLink className="navigation" to="/form">
-          Form
-        </NavLink>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+        <header className="header">
+          <h1 className="logo">ReactAPP</h1>
+          <NavLink className="navigation" to="/">
+            Home
+          </NavLink>
+          <NavLink className="navigation" to="/about">
+            About
+          </NavLink>
+          <NavLink className="navigation" to="/counter">
+            Counter
+          </NavLink>
+          <NavLink className="navigation" to="/form">
+            Form
+          </NavLink>
+        </header>
 
-      <main>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </main>
+        <main>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/counter" element={<Counter />} />
+            <Route path="/form" element={<FormPage />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
 
-      <footer>
-        <p> ReactAPP </p>
-      </footer>
-    </div>
+        <footer>
+          <p> 4Quark </p>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
